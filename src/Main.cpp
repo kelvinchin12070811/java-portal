0fecf6281a2155336a0c220f331a7b326d513a61
Kelvin Chin <chinkaiwen@yahoo.com>
2022-01-08 16:55:21 +0800

    updated CMakeLists and test with Unicode output with fmt

--- src/Main.cpp
+++ src/Main.cpp
@@ -3,12 +3,26 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  **********************************************************************************************************************/
+#include <array>
+#include <chrono>
 #include <string>
+#include <thread>
 
 #include <fmt/ostream.h>
 
 int main(int argc, char** argv)
 {
-    fmt::print("hello world");
+    const std::array<std::string, 4> loadingIconState = { "[█  ]", "[ █ ]", "[  █]", "[ █ ]" };
+    int loopState { 0 };
+
+    while (true)
+    {
+        using namespace std::chrono_literals;
+        fmt::print("{}\r", loadingIconState[loopState]);
+        std::this_thread::sleep_for(100ms);
+        loopState++;
+        if (loopState >= loadingIconState.size()) loopState = 0;
+    }
+
     return 0;
 }
